d53d48df510c8a74c0b4ff38ffb9ff55dd2d7f74
Alexis <amart78@outlook.com>
2022-11-18 15:17:06 -0600

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Alex on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
